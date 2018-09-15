cec84b59e3879abf8145c6978bc57f01911d210c
MaikelGeres <33796308+MaikelGeres@users.noreply.github.com>
2018-09-15 14:16:22 -0400

    Created a class to define defferent vehicle properties and current owner

--- vehicle.py
+++ vehicle.py
@@ -2,10 +2,36 @@
 vehicles common funtions and classes
 """
 
-def get_vh_num(vh_num):
-	""" """
-	return vh_num
+class vehicle:
+	"""
+	A class for the basic objects and methods for any vehicle
+	"""
+	
+	def __init__(self, sr, mk, mdl, yr, plt_num):
+		"""
+		Constructor to create a vhecile object with the make, model, and the plate number
+		"""
+		
+		# main props
+		self.vh_sn = str(sn)
+		self.vh_mk = str(mk)
+		self.vh_mk = str(mdl)
+		self.vh_yr = str(yr)
+		self.vh_mk = str(plt_num)
+		
+		# owners
+		self.vh_owners = []
+		
+	
+	def cur_owner_full_name(self, person_name):
+		"""
+		Sets the current vehicle owner full name
+		"""
+		
+		self.vh_owners.append(str(person_name))
+		
 
-def get_vh_make(vh_mk):
-	""" """
-	return vh_mk
+
+
+
+
